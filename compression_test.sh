275f5777d12054781ba243b39b65ab9bfbd26ccc
arijus303 <arijus.zickus@gmail.com>
2022-06-20 19:49:41 +0100

    compresses single file

--- compression_test.sh
+++ compression_test.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-compressProg=(gzipFUNC)
+compressProg=(gzipFUNC bzip2FUNC )
 #this is for total count of compression programs, all of them are going to be used so we need to take preliminary 
 #size of compression and multiply by total program count. This way we can check if we have enough space to 
 #per form compression tests.
 totalFuncNum=${#compressProg[@]}
-testDir="/tmp/compression_test/"
+testDir="/tmp/compression_test_$$/"
 i=10
 filename="compression_test."
 
 #check if file exists
 #ckeck if we have a total space
 
-#compression functions:
+#####################################################
+#COMPRESSION FUNCTIONS:
+#####################################################
 
 function gzipFUNC(){
+
 	echo "Gzip - attempting to compress."
 	id=$1 
 	gzip -c "$2" > "$testDir$id"
@@ -27,8 +30,27 @@ function gzipFUNC(){
 	
 }
 
+function bzip2FUNC(){
+
+	echo "Bzip - attempting to compress."
+	id=$1 
+	bzip2 -c "$2" > "$testDir$id"
+	STATUS=$?
+	if [[ $STATUS -eq 0 ]];then
+	       echo "Bzip2 - completed."
+       	else
+	       echo "Bzip2 - error while compressing."
+       	fi	       
+	
+}
+#######################################################
+#######################################################
+
+
 id=0
 mkdir -p $testDir
+trap 'rm -rf "$testDir"' EXIT
+
 
 for func in ${compressProg[@]}; do
 	
@@ -41,4 +63,3 @@ for func in ${compressProg[@]}; do
 		exit 1
 	fi
 done
-
