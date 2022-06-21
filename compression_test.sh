011c5865d42248e9672c0a696de339db94248fae
arijus303 <arijus.zickus@gmail.com>
2022-06-21 18:58:32 +0100

    test is working and displays best compression method

--- compression_test.sh
+++ compression_test.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-compressProg=(gzipFUNC bzip2FUNC )
+compressProg=(gzipFUNC bzip2FUNC) #<-
+compressProgName=(gzip bzip2) #<-
 #this is for total count of compression programs, all of them are going to be used so we need to take preliminary 
 #size of compression and multiply by total program count. This way we can check if we have enough space to 
 #per form compression tests.
 totalFuncNum=${#compressProg[@]}
 testDir="/tmp/compression_test_$$/"
 i=10
-#FILENAME=$1
-#check if file exists
 #ckeck if we have a total space
+
+if [[ $# -eq 0 ]]; then
+	echo "Usage: $0 [file/s name to test]" >&2
+	exit 1
+fi
 
 #####################################################
 #COMPRESSION FUNCTIONS:
@@ -17,38 +21,54 @@ i=10
 
 function gzipFUNC(){
 
-	echo "gzip - attempting to compress: $2"
+	local NAME=${compressProgName[$1]}
+
+	echo "$NAME - attempting to compress: $2"
 	id=$1 
-	gzip -c "$2" > "$testDir$id"
+	gzip -c "$2" > "$testDir$id" #<-
 	STATUS=$?
 	if [[ $STATUS -eq 0 ]];then
-	       echo "gzip - completed."
+	       echo "$NAME - completed."
        	else
-	       echo "gzip - error while compressing."
+	       echo "$NAME - error while compressing."
        	fi	       
 	
 }
 
 function bzip2FUNC(){
 
-	echo "bzip2 - attempting to compress: $2"
+	local NAME=${compressProgName[$1]}
+
+	echo "$NAME - attempting to compress: $2"
 	id=$1 
 	bzip2 -c "$2" > "$testDir$id"
 	STATUS=$?
 	if [[ $STATUS -eq 0 ]];then
-	       echo "bzip2 - completed."
+	       echo "$NAME - completed."
        	else
-	       echo "bzip2 - error while compressing."
+	       echo "$NAME - error while compressing."
        	fi	       
 	
 }
 #######################################################
+#COMPRESSION FUNCTIONS END
 #######################################################
+
+function compare() {
+
+        local __FILENAME=$1
+        echo "Comparing @ $testDir"
+        smallest_size=`ls -l $testDir | awk 'NR>1{print $5}' | sort -n | head -1`
+        functionID=`ls -l $testDir | grep $smallest_size | awk '{print $9}'`
+
+        
+        echo "Best compression for file $__FILENAME is ${compressProgName[$functionID]}"
+}
 
 
 id=0
 mkdir -p $testDir
-trap 'rm -rf "$testDir"' EXIT
+trap 'rm -rf "$testDir"' EXIT #clean up after test.
 
 for FILENAME in $@; do
 
@@ -57,11 +77,12 @@ for FILENAME in $@; do
 
 	for func in ${compressProg[@]}; do
 	
-		$func $id $FILENAME
+		check_for_space $id #check for space availability
+		$func $id $FILENAME #function call to compress
 		((id++))
 			
 		done
-		#<- write a comparison function and append result to log.
+		compare $FILENAME 
 	else
 		echo "File [ $FILENAME ] does not exist!, please check and try again" >&2
 		continue
